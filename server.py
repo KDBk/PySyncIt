73e39b252f3f5cb421b7ccb2a5dbb727f7a39a4a
Dai Dang Van <daikk115@gmail.com>
2017-05-18 19:09:43 +0700

    Important | preserve file attributes

--- server.py
+++ server.py
@@ -91,7 +91,7 @@ class Server(Node):
 
     def push_file(self, filename, dest_file, dest_uname, dest_ip):
         """push file 'filename' to the destination"""
-        command = "{} -q -l {} -pw {} {} {}@{}:{}".format(
+        command = "{} -qp -l {} -pw {} {} {}@{}:{}".format(
             PSCP_COMMAND[ENV], self.username, self.passwd,
             filename, dest_uname, dest_ip, dest_file).split()
         print(command)
@@ -105,7 +105,7 @@ class Server(Node):
         """pull file 'filename' from the source"""
         my_file = "{}{}".format(self.watch_dirs[0], filename)
         self.pulled_files.add(my_file)
-        command = "{} -q -l {} -pw {} {}@{}:{} {}".format(
+        command = "{} -qp -l {} -pw {} {}@{}:{} {}".format(
             PSCP_COMMAND[ENV], self.username, self.passwd, source_uname,
             source_ip, source_file, my_file).split()
         print(command)
